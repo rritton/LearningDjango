290495b0c6478913b0cdd0aa1f68582caa341846
norit <bart.daude@gmail.com>
2018-12-09 16:31:37 +0100

    mise en place d'un index html

--- BasicProject/HelloDjangoApp/views.py
+++ BasicProject/HelloDjangoApp/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("Hello, Django !")
+    now = datetime.now()
+    
+    return render(
+        request,
+        "HelloDjangoApp/index.html",
+        {#liste des variables
+            'title': "Hello Django",
+            'message': "Hello Django !",
+            'content': " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
